20ec13a540dbcc780541c53fe97916c7aac49e5c
Jakub Pietrzak <jpietrzi@gmail.com>
2021-02-21 10:38:20 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+const LearnReactBanner = ({topic}) => {
+  return ( <a
+    className="App-link"
+    href="https://reactjs.org"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    Learn React 
+    </a>);
+}
+
 function App() {
   return (
     <div className="App">
@@ -9,14 +21,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <LearnReactBanner topic="iTunes browser in React"/>
       </header>
     </div>
   );
